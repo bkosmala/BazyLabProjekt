0ecc7198cb9ba5184a9c23966044c53d37035635
195669 Olejniczak Maciej <195669@edu.p.lodz.pl>
2016-11-30 15:26:50 +0100

    dodanie tabel: Kategoria i Sprzet

--- SQL/CreateDatabaseScript.sql
+++ SQL/CreateDatabaseScript.sql
@@ -26,3 +26,22 @@ idCennik int not null,
 CONSTRAINT Znizka_FK FOREIGN KEY(idZnizka) REFERENCES Znizki(idZnizka), 
 CONSTRAINT Cennik_FK FOREIGN KEY(idCennik) REFERENCES Cenniki(idCennik)
 )
+
+CREATE TABLE Kategoria 
+(
+id_kategorii INT IDENTITY(1,1) CONSTRAINT kategoria_PK PRIMARY KEY,
+id_cennika INT not null,
+nazwa_kat VARCHAR(40) NOT NULL,
+CONSTRAINT cennik_FK FOREIGN KEY(id_cennika) REFERENCES Cenniki(idCennik)
+)
+
+CREATE TABLE Sprzet
+(
+id_sprzetu INT IDENTITY(1,1) CONSTRAINT Sprzet_PK PRIMARY KEY,
+id_kat INT not null,
+nazwa VARCHAR(20) NOT NULL,
+stan VARCHAR(20) NOT NULL,
+sprawnosc VARCHAR(40) NOT NULL,
+opis VARCHAR(255),
+CONSTRAINT kategoria_FK FOREIGN KEY(id_kat) REFERENCES Kategoria(id_kategorii)
+)
