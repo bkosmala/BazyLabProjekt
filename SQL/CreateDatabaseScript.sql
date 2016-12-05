27f49ab6ae135ecfab3079e859800fc52572b7d7
bkosmala <b.kosmala@interia.pl>
2016-12-05 21:55:03 +0100

    poprawki

--- SQL/CreateDatabaseScript.sql
+++ SQL/CreateDatabaseScript.sql
@@ -5,7 +5,7 @@ GO
 
 CREATE TABLE Znizki
 (
-idZnizka INT IDENTITY(1,1) CONSTRAINT Znizki_PK PRIMARY KEY,
+id_Znizka INT IDENTITY(1,1) CONSTRAINT Znizki_PK PRIMARY KEY,
 nazwa varchar(50) not null,
 wartosc float not null -- wartosc procentowa zapisna jako float
 CONSTRAINT wartosc_znizki_jako_procent CHECK(wartosc >=0 and wartosc<=1)
@@ -13,21 +13,21 @@ CONSTRAINT wartosc_znizki_jako_procent CHECK(wartosc >=0 and wartosc<=1)
 
 CREATE TABLE Cenniki
 (
-idCennik INT IDENTITY(1,1) CONSTRAINT Znizki_PK PRIMARY KEY,
-cenaPodstawowa float not null,
+id_Cennik INT IDENTITY(1,1) CONSTRAINT Znizki_PK PRIMARY KEY,
+cena_Podstawowa float not null,
 waluta varchar(3) not null,
-CONSTRAINT cena_nie_ujemna CHECK(cenaPodstawowa>=0)
+CONSTRAINT cena_nie_ujemna CHECK(cena_Podstawowa>=0)
 )
 
 CREATE TABLE CennikZnizki
 (
 idZnizka int not null,
 idCennik int not null,
-CONSTRAINT Znizka_FK FOREIGN KEY(idZnizka) REFERENCES Znizki(idZnizka), 
-CONSTRAINT Cennik_FK FOREIGN KEY(idCennik) REFERENCES Cenniki(idCennik)
+CONSTRAINT Znizka_FK FOREIGN KEY(idZnizka) REFERENCES Znizki(id_Znizka), 
+CONSTRAINT Cennik_FK FOREIGN KEY(idCennik) REFERENCES Cenniki(id_Cennik)
 )
 
-CREATE TABLE Kategoria 
+CREATE TABLE Kategorie_Sprzetu 
 (
 id_kategorii INT IDENTITY(1,1) CONSTRAINT kategoria_PK PRIMARY KEY,
 id_cennika INT not null,
@@ -40,18 +40,19 @@ CREATE TABLE Sprzet
 id_sprzetu INT IDENTITY(1,1) CONSTRAINT sprzet_PK PRIMARY KEY,
 id_kat INT not null,
 nazwa VARCHAR(20) NOT NULL,
-stan VARCHAR(20) NOT NULL,
-sprawnosc VARCHAR(40) NOT NULL,
-opis VARCHAR(255),
-CONSTRAINT kategoria_FK FOREIGN KEY(id_kat) REFERENCES Kategoria(id_kategorii)
+stan VARCHAR(20) NULL,
+sprawnosc VARCHAR(40) NULL,
+opis VARCHAR(255) NULL,
+CONSTRAINT kategoria_FK FOREIGN KEY(id_kat) REFERENCES Kategorie_Sprzetu(id_kategorii)
 )
 
 CREATE TABLE Wypozyczenie
 (
 id_wypozyczenia INT IDENTITY(1,1) CONSTRAINT wypozyczenie_PK PRIMARY KEY,
-data_wypozyczenia TIMESTAMP NOT NULL,
-data_zwrotu TIMESTAMP NOT NULL, 
-CONSTRAINT wypoz_check_daty CHECK(data_wypozyczenia < data_zwrotu)
+data_wypozyczenia DATETIME NOT NULL,
+data_zwrotu DATETIME NULL, 
+uwagi VARCHAR(255) NULL,
+CONSTRAINT wypoz_check_daty CHECK(data_wypozyczenia < isnull(data_zwrotu,dateadd(y,1,data_wypozyczenia)
 )
 
 CREATE TABLE Rezerwacja
